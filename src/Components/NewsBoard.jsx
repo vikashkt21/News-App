e77b70173a5d621af8e4fddae1272107ae8b743d
salonikatiyar123 <saloni.katiyar@telusinternational.com>
2024-07-02 15:43:21 +0530

    first commit

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import NewsItem from "./NewsItem";
+
+
+const NewsBoard = ({category}) => {
+    const [articles,setArticles]=useState([]);
+    useEffect(()=>{
+        let url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=8d39f8fae5e442c39ad6d9a4a9385c1b`
+        fetch(url).then(response=>response.json()).then(data=>setArticles(data.articles));
+    },[category])
+  return (
+    <div>
+      <h2 className="text-center">Latest<span className="badge bg-danger">News</span></h2>
+      {articles.map((news,index)=>{
+        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+      })}
+    </div>
+  )
+}
+
+export default NewsBoard
+ 
